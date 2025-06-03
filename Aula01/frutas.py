202b7e975e655e9820c5c616af6cf1e8c67fad05
RandGusta <gustarandiiii@gmail.com>
2025-06-03 12:00:34 -0300

    Update frutas.py

--- Aula01/frutas.py
+++ Aula01/frutas.py
@@ -1,4 +1,4 @@
-# %%
+
 import pandas as pd
 
 df = pd.read_excel(r"C:\Users\gusta\OneDrive\Área de Trabalho\ml-4-poneis-main\ml-4-poneis-main\data\drive-download-20250528T163348Z-1-001\dados_frutas.xlsx")
@@ -40,3 +40,22 @@ arvore.fit(x,y) # fit = aprenda, aprenda com os meus dados no caso X e Y --> ISS
 
 tree.plot_tree(arvore, class_names= arvore.classes_, feature_names= features, filled=True) # usamos o metodo plot tree e passamos o objeto "arvore" --> MOSTRA OQUE APRENDEU 
 # imagem da arvore --> na pasta imagens
+
+arvore.predict([[0, 1, 1, 1]]) # --> mostra a um resultado que bate com essas 'features'
+# ([["Arredondada", "Suculenta", "Vermelha", "Doce"]])
+# trabalhando com os features definidos na mesma ordem
+# 1 = true e 0 = false
+
+prob = arvore.predict_proba([[0, 1, 1, 1]])[0] # --> faz uma lista de probabilidade --> UMA probabibilidade para CADA classe
+# ou seja uma lista com a probabilidade de cada fruta 
+
+pd.Series(prob, index = arvore.classes_)
+# Cria uma "Lista" --> com a probabilidade de cada classe de acordo com nosso ARRAY "prob"
+# Resultado da previsão (probabilidades para cada fruta):
+# Banana     0.0
+# Cereja     0.5
+# Limão      0.0
+# Maçã       0.5
+# Morango    0.0
+# Pera       0.0
+# Tomate     0.0
